f11841df4946608334ba83376b8de8eabf2566b1
Veljko Ilic <veljkoilic93@hotmail.com>
2022-07-06 12:29:53 +0200

    minimal comment component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { Comment } from "./components/Comment";
+import data from "./data.json"
+
 function App() {
+  const [comments, setComments] = useState(data.comments)
   return (
-    <div className="App">
-      
-    </div>
+    <Container className="App">
+      {comments.map(c=><Comment key={c.id} comment={c}/>)}
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+
+display: flex;
+align-items: center;
+flex-direction: column;
+margin: 50px auto;
+`
